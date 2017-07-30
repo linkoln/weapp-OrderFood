27be532b81378fd331902baae4c16d6fda3100e0
linkoln <642704882@qq.com>
2017-07-30 23:28:19 +0800

    初始工程

---
+++ page/menu/menu.js
@@ -0,0 +1,73 @@
+// menu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menulists: [
+      {
+        "name":"mineral_water",
+        "image":"http://i4.eiimg.com/1949/dff1e43715a1639c.jpg",
+        "price":"2.5",
+        "desc":"矿泉水"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    // console.log(menulists)
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
